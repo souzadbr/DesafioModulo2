5d97db6afb115d2afcbf424ea6c5ee660aef41c6
Debora Souza <deboral.p@hotmail.com>
2021-09-03 14:45:59 -0300

    Implementação de barrar CPF's iguais e inserção de comentarios

--- src/br/com/zup/Desafio_Modulo2.java
+++ src/br/com/zup/Desafio_Modulo2.java
@@ -15,12 +15,12 @@ public class Desafio_Modulo2 {
             System.out.println("Menu inicial");
             System.out.println("Digite 1: Para cadastrar funcionários.");
             System.out.println("Digite 2: Para exibir lista de funcionários.");
-            System.out.println("Digite 3: Para Excluir o cadastro de funcionário.");
+            System.out.println("Digite 3: Para excluir o cadastro de funcionário.");
             System.out.println("Digite 4: Para encerrar interação com o programa.");
             int selecionar = 0;
 
-
-            System.out.println("Digite a opção desejada:");
+            //Inicio da interação com o programa
+            System.out.println("Digite a opção desejada: ");
             selecionar = leitor.nextInt();
             leitor.nextLine();
 
@@ -36,6 +36,13 @@ public class Desafio_Modulo2 {
                     System.out.println("Digite o CPF do funcionário: ");
                     String cpf = leitor.nextLine();
 
+                    //Validação de CPF para barrar CPF iguais.
+                    if (funcionario.containsKey(cpf)) {
+                        System.out.println("Esse CPF já consta no sistema!");
+                        System.out.println("Digite um CPF válido:");
+                        cpf = leitor.nextLine();
+                    }
+
                     //Fazendo cadastro
                     funcionario.put(cpf, " - Nome: " + nome + "- E-mail: " + email + " - Telefone: " + telefone);
 
@@ -47,14 +54,14 @@ public class Desafio_Modulo2 {
 
                 case 2:
                     //Listando funcionário cadastrados
-                   if (funcionario.isEmpty()) {
-                       System.out.print("Lista Vazia, retorne ao Menu e Cadastre um funcionário."); //Sinaliza lista vazia .isEmpty
+                    if (funcionario.isEmpty()) {
+                        System.out.print("Lista Vazia, retorne ao Menu e Cadastre um funcionário."); //Sinaliza lista vazia .isEmpty
 
-                   }else {
-                       for (String chaveFuncionario : funcionario.keySet()) {
-                           System.out.println(funcionario.get(chaveFuncionario) + " CPF: " + chaveFuncionario);//Exibi a lista de funcionarios e seus dados cadastrados.
-                       }
-                   }
+                    } else {
+                        for (String chaveFuncionario : funcionario.keySet()) {
+                            System.out.println(funcionario.get(chaveFuncionario) + " CPF: " + chaveFuncionario);//Exibi a lista de funcionarios e seus dados cadastrados.
+                        }
+                    }
                     System.out.println("Digite 1 para voltar ao Menu inicial e 0 para finalizar!");
                     retorno = leitor.nextInt();
                     break;
@@ -82,7 +89,7 @@ public class Desafio_Modulo2 {
 
 
         } while (retorno == 1); //Para retornar ao Menu ou sair caso deseje.
-   //retorno igual a 1 portanto, se digitado 1 ele retorna ao menu inicial e se digitado 0 que é != de 1 ele encerra o programa
+        //retorno igual a 1 portanto, se digitado 1 ele retorna ao menu inicial e se digitado 0 que é != de 1 ele encerra o programa
 
     }
 }
