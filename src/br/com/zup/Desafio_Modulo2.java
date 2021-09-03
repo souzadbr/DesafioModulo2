28c8c5b673a7fdf2662b28d7ef28e65fb56d4875
Debora Souza <deboral.p@hotmail.com>
2021-09-03 11:07:15 -0300

    Instaciando Scanner e HashMap

--- src/br/com/zup/Desafio_Modulo2.java
+++ src/br/com/zup/Desafio_Modulo2.java
@@ -1,10 +1,13 @@
 package br.com.zup;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Desafio_Modulo2 {
 
     public static void main(String[] args) {
-
+        Scanner leitor = new Scanner(System.in);
+        Map<String, String>funcionario = new HashMap<String,String>();
     }
 }
