da66032d66b395d18ce622b6a524f18ddd8e6b7d
Debora Souza <deboral.p@hotmail.com>
2021-09-03 11:22:16 -0300

    Receber dados cadastro e cadastrar 1 funcionário

--- src/br/com/zup/Desafio_Modulo2.java
+++ src/br/com/zup/Desafio_Modulo2.java
@@ -8,6 +8,22 @@ public class Desafio_Modulo2 {
 
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        Map<String, String>funcionario = new HashMap<String,String>();
+        Map<String, String> funcionario = new HashMap<String,String>();
+
+        //Cadastrar um funcionário
+        System.out.println("Digite o nome completo do funcionário: ");
+        String nome = leitor.nextLine();
+        System.out.println("Digite o telefone do funcionário: ");
+        String telefone = leitor.nextLine();
+        System.out.println("Digite o e-mail do funcionário: ");
+        String email = leitor.nextLine();
+        System.out.println("Digite o CPF do funcionário: ");
+        String cpf = leitor.nextLine();
+
+        //Fazendo cadastro
+        funcionario.put(cpf," - Nome: "+ nome + "- E-mail: "+email + " - Telefone: "+telefone);
+        System.out.println("A lista de funcionários cadastrado é: "+ funcionario);
+
+
     }
 }
